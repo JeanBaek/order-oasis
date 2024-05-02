88932d6cee9f65addd39308527342cc0d42a7596
jeanbaek <jeanbaek710@gmail.com>
2024-05-02 15:46:26 +0900

    feat: 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="w-screen h-screen p-3">
+          <div className="w-full h-full border border-black">
+              Order Oasis
+          </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("Failed to find the root element");
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
